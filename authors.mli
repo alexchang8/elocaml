090157d7b0de329fe093031dfbf61a6f22128877
Evan Pike <dep78@cornell.edu>
2019-04-10 20:07:20 -0400

    More init files

---
+++ authors.mli
@@ -0,0 +1,9 @@
+(**
+   Metadata about this submission.
+   @author All author names and NetIDs. 
+
+*)
+
+(** [hours_worked] is a list of the number of hours each team member
+    worked on this assignment. *)
+val hours_worked : int list
