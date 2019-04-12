c96996064462bd055eea9d0d9ba523478c032a04
Evan Pike <dep78@cornell.edu>
2019-04-11 20:36:20 -0400

    Implemented insertion of ships

--- player.ml
+++ player.ml
@@ -1,9 +1,10 @@
 
-type cell = Empty|Ship|Miss|Hit
+type cell = Empty|Ship|Miss|Hit|Sunk
 type board = cell list list
 type coord = Hit | Coord of int * int
 type ship = {sunk :bool ; size:int; inserted:bool; coords: coord list}
 
+(** RI: Board shape can be no larger than 26 by 26 *)
 type player = {
   board : board;
   ships : ship list;
@@ -11,6 +12,19 @@ type player = {
 }
 
 type  t = player
+
+let alphabet = " ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+(** [index c] is the 0-based index of [c] in the alphabet.
+    Requires: [c] is an uppercase letter in A..Z. *)
+let index (c:char) : int =
+  String.index alphabet c
+
+(** [inv_index n] takes an index value and returns the corresponding letter
+    in the alphabet.
+    Requires: [n] is an int between 0-25. *)
+let inv_index (n:int) : char = 
+  String.get alphabet n
 
 let rec set_inner (inner_acc:cell list) (y:int) = 
   if y > 0 
@@ -25,29 +39,60 @@ let init_empty_board x y =
     else acc
   in set_outer [] x y
 
-let init_ship size =
-  {
-    sunk=false;
-    size=size;
-    inserted=false;
-    coords=[]
-  }
-
-let init_ships ship_sizes = 
-  let rec tr_helper acc sships = 
-    match sships with
-    | [] -> acc 
-    | h::t -> tr_helper ((init_ship h)::acc) t
-  in tr_helper [] ship_sizes
-
 let init_shape x y = 
   (x, y)
 
 let init_player x y ship_sizes = 
   {
     board = init_empty_board x y;
-    ships = init_ships ship_sizes;
+    ships = [];
     shape = init_shape x y
+  }
+
+let add_ship ships coords = 
+  let new_ship = 
+    {
+      sunk = false;
+      size = List.length coords;
+      inserted = true;
+      coords = coords
+    } in
+  new_ship::ships
+
+let empty_cell c = c = Empty
+
+let add_ship_to_board board coords = 
+  let rec inner_loop acc row_pos col_pos row =
+    match row with
+    | [] -> acc
+    | h::t -> 
+      begin
+        if List.mem (Coord (row_pos, col_pos)) coords
+        then begin 
+          if not (empty_cell h) 
+          then failwith "BLAH" (* Add real exception *)
+          else inner_loop (Ship::acc) row_pos (col_pos+1) t
+        end
+        else inner_loop (h::acc) row_pos (col_pos+1) t
+      end in
+  let rec outer_loop acc row_pos b =
+    match b with
+    | [] -> acc
+    | h::t -> begin 
+        outer_loop ((List.rev (inner_loop [] row_pos 1 h))::acc) (row_pos+1) t
+      end
+  in List.rev (outer_loop [] 1 board)
+
+let get_all_cords start_cord end_cord = [Coord (1,1); Coord (1,2); Coord (1,3)]
+
+let insert_ship player start_cord end_cord = 
+  let cord_list = get_all_cords start_cord end_cord in
+  let new_ships = add_ship player.ships cord_list in
+  let new_board = add_ship_to_board player.board cord_list in
+  {
+    board=new_board;
+    ships=new_ships;
+    shape=player.shape
   }
 
 (** [cell_spacing] is a constant representing the space between cells on a
@@ -66,14 +111,22 @@ let get_dash_space () =
     space. Incorporated to display the game to the user. *)
 let dash_space = get_dash_space ()
 
-let get_row (row:cell list) = 
+let parse_cell c = 
+  match c with
+  | Empty -> " "
+  | Ship -> " "
+  | Miss -> "0"
+  | Hit ->  "X"
+  | Sunk -> "Z"
+
+let get_row (row_acc:int) (row:cell list) = 
   let rec tr_helper acc r = 
     match r with 
     | [] -> acc
-    | h::t -> 
-  in get_row "" row
+    | h::t -> tr_helper (acc ^ (parse_cell h ^ cell_spacing)) t
+  in tr_helper ((row_acc |> inv_index |> Char.escaped) ^ cell_spacing) row
 
-let print_row row = get_row row |> print_endline
+let print_row row_acc row = get_row row_acc row |> print_endline
 
 let rec print_dash row_size = 
   if row_size > 0
@@ -91,16 +144,18 @@ let print_top_labels s =
     else print_endline cell_spacing
   in helper 1 s
 
-let temp_player = init_player 3 3 [1;2;3]
+let temp_player = init_player 5 5 [1;2;3]
 
 let print_board player = 
   let shape = player.shape in
   let max_size = (snd shape) in (* change if we extend to unique boards *)
   let () = print_top_labels max_size in
-  let rec intermediate (cur_board:board) =
+  let rec intermediate (row_acc:int) (cur_board:board) =
     match cur_board with
-    | [] -> begin (snd shape + 1) |> print_dash; print_endline "" end
+    | [] -> begin (fst shape + 1) |> print_dash; print_endline "" end
     | h::t -> begin
-        (snd shape + 1) |> print_dash; print_row h; intermediate t
+        (fst shape + 1) |> print_dash; 
+        print_row row_acc h; 
+        intermediate (row_acc+1) t
       end
-  in intermediate player.board
+  in intermediate 1 player.board
