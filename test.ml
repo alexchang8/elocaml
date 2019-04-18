5c592796925ed527fbc520237bfcc0e50c2bbf6f
Alex Chang <apc73@cornell.edu>
2019-04-17 23:14:24 -0400

    update tests

--- test.ml
+++ test.ml
@@ -9,25 +9,25 @@ let init_player_test
     (name:string)
     (x:int)
     (y:int)
-    (p_name:int) 
-    (player:Player.t) : test = 
-  name >:: (fun _ -> 
+    (p_name:int)
+    (player:Player.t) : test =
+  name >:: (fun _ ->
       assert_equal p_name (get_name player);
       assert_equal (x, y) (get_shape player);
       assert_equal [] (get_ships player) )
 
-let insert_ship_test 
+let insert_ship_test
     (name:string)
     (board:board)
     (b:board) : test =
-  name >:: (fun _ -> 
+  name >:: (fun _ ->
       assert_equal b board;)
 
-let already_guessed_test 
+let already_guessed_test
     (name:string)
     (p:Player.t)
     (c:(int*int))
-    (x:bool) :test = 
+    (x:bool) :test =
   name >:: (fun _ ->
       assert_equal (already_guessed p c) x)
 
@@ -50,13 +50,13 @@ let four_piece_h = [[Empty; Empty; Empty; Empty; Empty]; [Empty; Ship; Ship; Shi
                     [Empty; Empty; Empty; Empty; Empty]]
 let ins_4_ship = insert_ship p_5by5_1 (make_coord (2,2)) (make_coord (5,2)) 4
 
-let give_t v = 
+let give_t v =
   match v with
   |ValidB t-> t
   |_-> failwith "not important"
 
 let var1 = give_t ins_3_ship
-let var2 = give_t ins_4_ship 
+let var2 = give_t ins_4_ship
 
 let guess1 = check var2 (2,2)
 let guess2 = check var2 (5,5)
@@ -64,7 +64,7 @@ let guess3 = check var1 (1,1)
 let guess4 = check var1 (1,2)
 let guess5 = check var1 (1,3)
 
-let give_t_2 g = 
+let give_t_2 g =
   match g with
   |Continue (t, _) -> t
   |_->failwith "not important"
@@ -81,30 +81,30 @@ let insert_tests = [
 let already_guessed_tests = [
   already_guessed_test "Has not guessed A1" var1 (1,1) false;
   already_guessed_test "Has not guessed A2" var1 (1,2) false;
-  already_guessed_test "Guessed B2" var3 (2,2) true; 
-  already_guessed_test "Guessed B2" var4 (2,2) false; 
+  already_guessed_test "Guessed B2" var3 (2,2) true;
+  already_guessed_test "Guessed B2" var4 (2,2) false;
   already_guessed_test "Guesses and Sunk" var5 (3,3) false;
 ]
 
-let string_form_test 
+let string_form_test
     (name:string)
     (s:string)
     (expected:bool) : test =
-  name >:: (fun _ -> 
+  name >:: (fun _ ->
       assert_equal expected (check_string_form s) ~printer:string_of_bool;
     )
 
 let string_list_test
     (name:string)
     (sl:string list)
-    (exp:bool) : test = 
-  name >:: (fun _ -> 
+    (exp:bool) : test =
+  name >:: (fun _ ->
       assert_equal exp (check_list_form sl) ~printer:string_of_bool)
 
 let parse_test
     (name:string)
     (s:string)
-    (exp:Command.command) : test = 
+    (exp:Command.command) : test =
   name >:: (fun _ ->
       assert_equal exp (parse s))
 
@@ -187,4 +187,51 @@ let suite = "Battleship Test Suite" >::: List.flatten [
     command_tests
   ]
 
-let _ = run_test_tt_main suite
+let go_parse_test (name:string) (s: string) (e: Go.command) : test =
+  name >:: (fun _ -> assert_equal (Go.parse s) e;)
+
+
+let next_state_test (name: string) (b: Go.cell array array) (e: Go.cell array array) : test =
+  let t = {Go.init_state with board = b} in
+  name >:: (fun _ -> assert_equal e ((Go.next_state t 0 (Go.Place(9,9)))).board)
+
+let go_command_tests = [
+  go_parse_test "pass" "pass" Pass;
+  go_parse_test "invalid test" "ksdjb" Invalid;
+  go_parse_test "lowercase test" "a1" Invalid;
+  go_parse_test "out of bounds test number" "a17" Invalid;
+  go_parse_test "out of bounds test letter" "U7" Invalid;
+  go_parse_test "normal test 1" "A19" (Go.Place (0,0));
+  go_parse_test "normal test 2" "A1" (Go.Place (18,0));
+  go_parse_test "normal test 3" "K10" (Go.Place (9,9));
+  go_parse_test "out of bounds test letter" "I7" Invalid;
+  go_parse_test "normal test 4" "Q16" (Go.Place (3,15));
+  go_parse_test "double test" "A1 A1" Invalid;
+]
+
+let b1 = (Array.copy Go.init_board)
+let _ =
+  b1.(0).(0) <- Go.Black;
+  b1.(0).(1) <- Go.Black;
+  b1.(1).(0) <- Go.White;
+  b1.(1).(1) <- Go.White;
+  b1.(0).(2) <- Go.White
+let b2 = (Array.copy Go.init_board)
+let _ =
+  b2.(1).(0) <- Go.White;
+  b2.(1).(1) <- Go.White;
+  b2.(0).(2) <- Go.White;
+  b2.(9).(9) <- Go.White
+
+let go_board_tests = [
+  next_state_test "remove test" b1 b2
+]
+
+let go_suite = "Go Test Suite" >::: List.flatten [
+    go_command_tests;
+    go_board_tests
+  ]
+
+let _ =
+  run_test_tt_main suite;
+  run_test_tt_main go_suite
