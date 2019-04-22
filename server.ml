fd4cd7ac2fe7454fca0b2957348820a9cfebe802
Alex Chang <apc73@cornell.edu>
2019-04-22 15:13:01 -0400

    start splitting view with chat

--- server.ml
+++ server.ml
@@ -9,7 +9,8 @@ end
 module MakeServer (G:Game) = struct
   (**A type that holds the input channel, output channel, and player id
      of a single client*)
-  type connection = {ic: in_channel; oc: out_channel; p_id: int}
+  type connection = {ic: in_channel; oc: out_channel; p_id: int; in_chat: bool;
+                     end_cursor: string}
   (**The port that the server will listen for players on*)
   let port = 1400
 
@@ -27,10 +28,41 @@ module MakeServer (G:Game) = struct
         if 1 + List.length acc < G.max_players then
           (output_string oc "waiting for more players\nEND_OF_FILE\n";
            flush oc);
-        let player = {ic = ic; oc = oc; p_id = List.length acc} in
+        let player = {ic = ic; oc = oc; p_id = List.length acc; in_chat = false; end_cursor = ""} in
         client_helper sock (player::acc)
       else acc in
     client_helper sock []
+
+  let pad_s_list n s s1 =
+    let rec helper n s1 =
+      if List.length s1 >= n then s1
+      else helper n (s::s1)
+    in List.rev s1 |> helper n |> List.rev
+
+  let split_n = String.split_on_char '\n'
+
+  let split_view_string s1 s2 =
+    let s1',s2' =
+      let s1_l = split_n s1 and
+      s2_l = split_n s2 in
+      let ns1_l = List.length s1_l and
+        ns2_l = List.length s2_l in
+      let n = ns1_l - ns2_l in
+      if n > 0 then
+        (s1_l, pad_s_list ns1_l "" s2_l)
+      else if n < 0 then
+        (pad_s_list ns2_l "                         " s1_l, s2_l)
+      else (s1_l,s2_l)
+    in List.map2 (fun x y -> x ^ "   \226\148\130   " ^ y) s1' s2' |> String.concat "\n"
+
+  let is_chat_click s =
+    match String.split_on_char ' ' s with
+    | x::cs::rs::[] when x = "mouse" -> begin
+        match int_of_string_opt cs, int_of_string_opt rs with
+        | Some c, Some r when c <= 28 -> true
+        | _ -> false
+      end
+    | _ -> false
 
   (**[c_next_state s conn] reads an input line from a particular client,
      and if available returns the result of parsing the line and finding the next
@@ -43,14 +75,19 @@ module MakeServer (G:Game) = struct
       (*TODO: wait for client reconnection*)
       failwith "unimplmented"
 
+  let top_view_string = " \226\143\187 │                                     " ^ G.name ^ "\n───┴────────────────────────────────────────────────────────────────────────────\n"
+
   (**[game_loop s conns] continuously reads all of the input channels in [conns]
      for input lines, and parses each of them into a command, and storing the mutated
      game state in sequence. If there are multiple connections with pending buffers,
      the order of this sequence is not gauranteed. This function will never terminate properly.*)
   let rec game_loop (s:G.t) conns =
     (*TODO: resolve when there are multiple endlines*)
+    (*TODO: use cursor movement instead of clearing the entire screen *)
+    (*TODO: pass mouse click offset instead of absolute position *)
     let s' = List.fold_left c_next_state s conns in
-    List.iter (fun c -> (G.print_player_state s c.p_id) ^ "\nEND_OF_FILE\n" |>
+    List.iter (fun c -> top_view_string ^ split_view_string "chat                     "
+                          (G.print_player_state s c.p_id) ^ "\nEND_OF_FILE\n" |>
                         output_string c.oc; flush c.oc) conns;
     Unix.sleepf 0.1;
     game_loop s' conns
